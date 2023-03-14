08a8d3692c7958d00ade115dbf8e0b924899fb18
Len <103252616+lena-mage-of-cod@users.noreply.github.com>
2023-03-14 15:39:41 +0300

    1 commit

---
+++ src/main/java/org/bokhoeva/config/MyConfig.java
@@ -0,0 +1,12 @@
+package org.bokhoeva.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("org.bokhoeva")
+@EnableWebMvc
+public class MyConfig   {
+}
---
+++ src/main/java/org/bokhoeva/config/MyDispatcherServlet.java
@@ -0,0 +1,26 @@
+package org.bokhoeva.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+
+public class MyDispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return  new Class[]{MyConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+}
